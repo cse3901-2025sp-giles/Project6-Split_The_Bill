d3ae68ef79092560d9a1dc684412e25fe8c6ceff
syyy44 <73976520+syyy44@users.noreply.github.com>
2025-04-15 16:10:57 +0800

    Fix: Apply RuboCop corrections and update HomeControllerTest for Devise

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers # Include Devise helpers
+
+  setup do
+    @user = users(:one) # Load the user fixture
+    sign_in @user       # Sign in the user
+  end
+
   test "should get index" do
-    get home_index_url
+    get root_url # Use root_url as defined in routes
     assert_response :success
   end
 end
