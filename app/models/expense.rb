81ef0d0d83dde3e6133a981c4862100400db5c42
root <root@SydneyPC>
2025-04-21 23:02:12 -0400

    Add styled trip dashboard, buttons, flash messages, and model validations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,6 +4,9 @@ class Expense < ApplicationRecord
   has_many :expense_participants, dependent: :destroy
   has_many :participants, through: :expense_participants
 
-  # this line allows Rails to save participant_ids from a form
   accepts_nested_attributes_for :expense_participants, allow_destroy: true
+
+  #basic validations
+  validates :description, :amount, :date, presence: true
+  validates :amount, numericality: { greater_than: 0 }
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,10 @@
 class Trip < ApplicationRecord
   belongs_to :user
   has_many :participants, dependent: :destroy
-  has_many :expenses, dependent: :destroy # 👈 add this line
+  has_many :expenses, dependent: :destroy
+
   accepts_nested_attributes_for :participants, allow_destroy: true, reject_if: proc { |attrs| attrs["name"].blank? }
+
+  #Basic validations
+  validates :name, :start_date, :end_date, presence: true
 end
